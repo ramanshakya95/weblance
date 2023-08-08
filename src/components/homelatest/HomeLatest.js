4fb4e39046210f37579ba04aa64dfbc72edd95a7
raman <shakyaraman29@gmail.com>
2023-08-08 17:43:07 +0530

    home latest view

--- src/components/homelatest/HomeLatest.js
+++ src/components/homelatest/HomeLatest.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
-import { latest_project_data } from '../../APIs/LatestProjectApi'
+import { latest_project_data } from '../../APIs/LatestProjectApi';
+import project1 from '../../assets/latestprojects/project1.jpg';
+import project2 from '../../assets/latestprojects/project2.jpg';
+import project3 from '../../assets/latestprojects/project3.jpg';
 import './HomeLatest.scss';
 
 const HomeLatest = () => {
@@ -22,7 +24,7 @@ const HomeLatest = () => {
                     <Row xs={1} md={2} lg={2} className='projects_ first_row'>
                         <Col className='_column'>
                             <Carousel
-                            draggable = {true}
+                                draggable={true}
                                 indicators={false}
                                 controls={true}
                                 autoPlay={true}
@@ -39,66 +41,22 @@ const HomeLatest = () => {
                                     </Carousel.Item>
                                 ))}
                             </Carousel>
+                            <h4>Website Designing & Development</h4>
                             {/* <img src={latest_project.image} alt='project' /> */}
                             {/* <h3>{latest_project.title}</h3> */}
                             {/* <h4><Link to='/'>{latest_project.subTitle}</Link></h4> */}
                         </Col>
                         <Col className='_column'>
-                            <Carousel
-                                indicators={false}
-                                controls={true}
-                                autoPlay={true}
-                                autoPlaySpeed={2000}
-                                infinite={true}
-                            >
-                                {latest_project_data.map(latest_project => (
-                                    <Carousel.Item key={latest_project.id}>
-                                        <img
-                                            className="d-block m-auto"
-                                            src={latest_project.image}
-                                            alt="testimonials"
-                                        />
-                                    </Carousel.Item>
-                                ))}
-                            </Carousel>
+                            <img src={project1} alt='project1' />
+                            <h4>Advertisements</h4>
                         </Col>
                         <Col className='_column'>
-                            <Carousel
-                                indicators={false}
-                                controls={true}
-                                autoPlay={true}
-                                autoPlaySpeed={2000}
-                                infinite={true}
-                            >
-                                {latest_project_data.map(latest_project => (
-                                    <Carousel.Item key={latest_project.id}>
-                                        <img
-                                            className="d-block m-auto"
-                                            src={latest_project.image}
-                                            alt="testimonials"
-                                        />
-                                    </Carousel.Item>
-                                ))}
-                            </Carousel>
+                            <img src={project2} alt='project2' />
+                            <h4>Business Card</h4>
                         </Col>
                         <Col className='_column'>
-                            <Carousel
-                                indicators={false}
-                                controls={true}
-                                autoPlay={true}
-                                autoPlaySpeed={2000}
-                                infinite={true}
-                            >
-                                {latest_project_data.map(latest_project => (
-                                    <Carousel.Item key={latest_project.id}>
-                                        <img
-                                            className="d-block m-auto"
-                                            src={latest_project.image}
-                                            alt="testimonials"
-                                        />
-                                    </Carousel.Item>
-                                ))}
-                            </Carousel>
+                            <img src={project3} alt='project3' />
+                            <h4>Logo Design</h4>
                         </Col>
                     </Row>
                 </Container>
