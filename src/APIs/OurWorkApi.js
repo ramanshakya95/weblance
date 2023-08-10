541d62d6d999c78969af81a38dafc16cdcabd51c
raman <shakyaraman29@gmail.com>
2023-08-10 15:15:35 +0530

    our work

--- src/APIs/OurWorkApi.js
+++ src/APIs/OurWorkApi.js
@@ -1,25 +1,41 @@
-export const category_data = 
+export const design_dev_data = 
 [
-{id: 1, category: 'Advertisements Design'},
-{id: 2, category: 'Brochure & Catalogue Design'},
-{id: 3, category: 'Business Cards Design'},
-{id: 4, category: 'Power Point Design'},
-{id: 5, category: 'Flyers Design'},
-{id: 6, category: 'Infographics & Flowcharts Design'},
-{id: 7, category: 'Label & Packaging Design'},
-{id: 8, category: 'Logo Design'},
-{id: 9, category: 'Social Media Banners Design'},
-{id: 10, category: 'Magazines Design'},
-{id: 11, category: 'Poster Design'},
-{id: 12, category: 'Standee & Billboards Design'},
-{id: 13, category: 'Music Album Covers Design'},
-{id: 14, category: 'Web Headers & Banners Design'},
-{id: 15, category: 'Stationery Design'},
-{id: 16, category: 'Invitation & Greetings Design'},
-{id: 17, category: 'Emailer & Newsletter Design'},
-{id: 18, category: 'Business Folder Design'},
-{id: 19, category: 'Menu Design'},
-{id: 20, category: 'Book Cover Design'},
-{id: 21, category: 'Postcards Design'},
-{id: 22, category: 'T-Shirts Design'},
+{id: 1, category: 'Wordpress Websites'},
+{id: 2, category: 'Woocommerce Websites'},
+{id: 3, category: 'Shopify'},
+{id: 4, category: 'Php Websites'},
+{id: 5, category: 'XD Templates'},
+{id: 6, category: 'Bootstrap Websites'},
+{id: 7, category: 'Emailer design'},
+{id: 8, category: 'Newsletter Design'},
+{id: 9, category: 'E-catalogues'}
+]
+
+export const graphic_data = [
+    {id: 1, category: 'Logo Design'},
+    {id: 2, category: 'Brochure Design'},
+    {id: 3, category: 'T-shirt Design'},
+    {id: 4, category: 'Infograhic Design'},
+    {id: 5, category: 'Business Card'},
+    {id: 6, category: 'Banner Design'},
+    {id: 7, category: 'Letter Heads'},
+    {id: 8, category: 'Animation'},
+    {id: 9, category: 'Menu Design'},
+    {id: 10, category: 'Label Design'},
+    {id: 11, category: 'Packaging Design'},
+    {id: 12, category: 'E-book Cover'},
+    {id: 13, category: 'Standy Design'},
+    {id: 14, category: 'PPT Designs'},
+    {id: 15, category: 'Flyer'},
+    {id: 16, category: 'Company Profile'}
+]
+
+export const content_data = [
+    {id: 1, category: 'Article Writing'},
+    {id: 2, category: 'Business Writing'},
+    {id: 3, category: 'Product Descriptions'},
+    {id: 4, category: 'website content writing'},
+    {id: 5, category: 'Blog Writing'},
+    {id: 6, category: 'Essay Writing'},
+    {id: 7, category: 'Proofreading'}
 ]
--- src/pages/ourwork/OurWork.js
+++ src/pages/ourwork/OurWork.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Container } from 'react-bootstrap';
-import { category_data } from '../../APIs/OurWorkApi';
+import { design_dev_data, graphic_data, content_data } from '../../APIs/OurWorkApi';
 import Banner from '../../components/banner/Banner';
-import WorkList from './WorkList';
+// import WorkList from './WorkList';
 import './ourwork.scss';
 
 const OurWork = () => {
@@ -13,18 +13,35 @@ const OurWork = () => {
             <section className='work_category_section'>
                 <Container className='p-0'>
                     <div className='work_category_row'>
-                        <div className='sidebar'>
-                            <h3>All Categories</h3>
+                        <div className='each_cat sidebar'>
+                            <h3>Web Design & Dev</h3>
                             <div className='category_list'>
                                 <ul>
-                                    {category_data.map(categories => (
+                                    {design_dev_data.map(categories => (
                                         <li key={categories.id}><Link to="#">{categories.category}</Link></li>
                                     ))}
                                 </ul>
                             </div>
                         </div>
-                        <div className='content'>
-                            <WorkList />
+                        <div className='each_cat sidebar'>
+                            <h3>Graphic Design</h3>
+                            <div className='category_list'>
+                                <ul>
+                                    {graphic_data.map(categories => (
+                                        <li key={categories.id}><Link to="#">{categories.category}</Link></li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='each_cat sidebar'>
+                            <h3>Content Writing</h3>
+                            <div className='category_list'>
+                                <ul>
+                                    {content_data.map(categories => (
+                                        <li key={categories.id}><Link to="#">{categories.category}</Link></li>
+                                    ))}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </Container>
