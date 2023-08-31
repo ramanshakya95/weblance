59bb348fd5c39941d6215d879b798443e54868ba
raman <shakyaraman29@gmail.com>
2023-08-31 10:42:08 +0530

    gg

--- src/pages/portfolio/Old_WorkList.js
+++ src/pages/portfolio/Old_WorkList.js
@@ -1,6 +1,6 @@
 import ReactPaginate from "react-paginate";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 // import ViewIcon from '../../assets/work/viewicon.png';
 import LinkIcon from '../../assets/work/linkicon.svg';
 import PagArrow from '../../assets/svg/pag-arrow.svg';
@@ -53,7 +53,7 @@ function WorkList() {
                         <div className='img_overlay'>
                             <img className='preview_img w-100' src={work.image} alt={work.title} loading='lazy' />
                             <div className='overlay_icons'>
-                                {/* <Link to="#" className="icons">
+                                {/* <Link to="#" className="icons chngs">
                                     <img src={ViewIcon} alt='view' />
                                 </Link> */}
                                 <Link to="#" className="icons">
